d72b12493ec19781a7073188245631b2134671f6
islam260301 <islam260301kg@gmail.com>
2021-05-27 11:21:43 +0600

    created navbar skeleton

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import style from './header.module.css';
+import {Navbar} from "../navbar/Navbar";
 
 export const Header = () => {
     return(
-        <div>
-            Header
+        <div className={style.header}>
+            <Navbar/>
         </div>
     )
 }
--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import style from './Navbar.module.css';
 
 export const Navbar = () => {
     return(
-        <div>
-            Navbar
-        </div>
+        <nav>
+            <ul>
+                <li className={style.nav_item}>events</li>
+                <li className={style.nav_item}>about</li>
+                <li className={style.nav_item}>for graduates</li>
+                <li className={style.nav_item}>for students</li>
+                <li className={style.nav_item}>graduates</li>
+                <li className={style.nav_item}>contacts</li>
+            </ul>
+        </nav>
     )
 }
