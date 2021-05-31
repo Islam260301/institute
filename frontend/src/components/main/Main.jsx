06e0a279c79004cf803e9b95435cb62a04f28d12
islam260301 <islam260301kg@gmail.com>
2021-05-31 13:57:15 +0600

    add Events item in Events.jsx

--- frontend/src/components/main/Main.jsx
+++ frontend/src/components/main/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Slide} from "./carousel/Carousel";
 import {Events} from "./events/Events";
+import {News} from "./news/News";
 
 export const Main = () => {
     return(
         <div>
             <Slide/>
             <Events/>
+            <News/>
         </div>
     )
 }
--- frontend/src/components/main/events/Events.jsx
+++ frontend/src/components/main/events/Events.jsx
@@ -1,27 +1,40 @@
 import React, {useState} from 'react';
 import Calendar from "react-calendar";
 import 'react-calendar/dist/Calendar.css';
+import style from "./events.module.css";
 import news_1 from "../../../assets/images/news_1.jpg";
 
 export function Events() {
     const [value, onChange] = useState(new Date());
+    const [date, setDate] = useState(new Date().getDate());
+    const [month, setMonth] = useState(new Date().toLocaleString('default', { month: 'long' }) );
+    const [hours, setHours] = useState(new Date().getHours());
+    const [minute, setMinutes] = useState(new Date().getMinutes());
+
+    const onChangeDate = (newDate) => {
+        onChange(newDate)
+        setDate(newDate.getDate())
+        setMonth(newDate.toLocaleString('default', { month: 'long' }))
+        setHours(newDate.getHours())
+        setMinutes(newDate.getMinutes())
+    }
 
     return (
-        <div className={"row"}>
+        <div className={"row " + style.events_container}>
             <div className={"col-12 mb-3 text-center"}>
                 <h2>Events of May</h2>
                 <hr/>
             </div>
-            <div className={"col-md-4 col-12"}>
+            <div className={"col-md-4 col-12 " + style.m_0 + style.event_item}>
                 <div className={"row"}>
-                    <img  src={news_1}/>
+                    <img className={style.events_img} src={news_1}/>
                 </div>
                 <div className={"row"}>
-                    <div className={"col-md-4 col-12"}>
+                    <div className={"col-md-4 col-12 " + style.events_time}>
                         <div className="meta-text text-center">
-                            <h3>26</h3>
-                            <p>мая</p>
-                            <span>15:30</span>
+                            <h3>{date}</h3>
+                            <p>{month}</p>
+                            <span>{`${hours}:${minute}`}</span>
                         </div>
                     </div>
                     <div className={"col-md-8 col-12"}>
@@ -29,7 +42,7 @@ export function Events() {
                             href="https://inai.kg/event/priglashaem-na-den-otkrytyh-dverey-dlya-abiturientov-v-onlayn-rezhime">В
                             КГИПИ День Открытых Дверей!</a></h6>
                             <ul>
-                                <li><i className="fa fa-user"></i> КГИПИ</li>
+                                <li><i className="fa fa-user"/> КГИПИ</li>
                             </ul>
                         </div>
                     </div>
@@ -40,10 +53,13 @@ export function Events() {
 
             <div className={"col-md-4 col-12"}>
                 <Calendar
-                    className={"col-md-4 col-12"}
-                    onChange={onChange}
+                    className={"col-md-4 col-12 " + style.m_0}
+                    onChange={(newDate) => onChangeDate(newDate)}
                     value={value}
                 />
+            </div>
+            <div className={`d-flex justify-content-center`}>
+                <button type="button" className={`btn btn-info`}>Show more</button>
             </div>
         </div>
     )
