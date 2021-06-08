adb7df34b69e2f30dbb418fea4a5ca73fb7f72dd
islamorozobaev <islam260301kg@gmail.com>
2021-06-09 01:27:31 +0600

    added personalCard logic & added delete logic to other endpoints

--- backend/routes/faculty.js
+++ backend/routes/faculty.js
@@ -1,1 +1,50 @@
-const router = require('express').Router();let Faculty = require('../models/faculty.model');router.route('/').get((req, res) => {    Faculty.find()        .then(faculties => res.json(faculties))        .catch(err => res.status(400).json(`Error ${err}`));});router.route('/add').post((req, res) => {    const {        name,        abbreviation,        description,        linkToWebSite,        image    } = req.body;    const newFaculty = new Faculty({        name,        abbreviation,        description,        linkToWebSite,        image    });    newFaculty.save()        .then((addedElement) => res.json('addedElement'))        .catch(err => res.status(400).json(`Error ${err}`));});module.exports = router;
+const router = require('express').Router();
+let Faculty = require('../models/faculty.model');
+
+
+
+router.route('/').get((req, res) => {
+    Faculty.find()
+        .then(faculties => res.json(faculties))
+        .catch(err => res.status(400).json(`Error ${err}`));
+});
+
+
+router.route('/').delete((req, res) => {
+    Faculty.findByIdAndDelete(req.query.id)
+        .then(data => res.json(
+            {
+                message: "Faculty was successfully deleted", data
+            }
+        ))
+        .catch(err => res.status(400).json(`Error ${err}`));
+});
+
+
+router.route('/add').post((req, res) => {
+    const {
+        name,
+        abbreviation,
+        description,
+        linkToWebSite,
+        image
+    } = req.body;
+
+    const newFaculty = new Faculty({
+        name,
+        abbreviation,
+        description,
+        linkToWebSite,
+        image
+    });
+
+    newFaculty.save()
+        .then((addedElement) => res.json(
+            {
+                message: "Faculty was successfully added", addedElement
+            }
+        ))
+        .catch(err => res.status(400).json(`Error ${err}`));
+});
+
+module.exports = router;
