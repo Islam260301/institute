008da9547d87f62141ee840ddeed4ab6fa97c68e
islamorozobaev <islam260301kg@gmail.com>
2021-06-09 23:11:35 +0600

    ended CRUD Faculty

--- backend/routes/faculty.js
+++ backend/routes/faculty.js
@@ -1,50 +1,10 @@
 const router = require('express').Router();
-let Faculty = require('../models/faculty.model');
+const facultyController = require('../controllers/facultyController');
 
 
-
-router.route('/').get((req, res) => {
-    Faculty.find()
-        .then(faculties => res.json(faculties))
-        .catch(err => res.status(400).json(`Error ${err}`));
-});
-
-
-router.route('/').delete((req, res) => {
-    Faculty.findByIdAndDelete(req.query.id)
-        .then(data => res.json(
-            {
-                message: "Faculty was successfully deleted", data
-            }
-        ))
-        .catch(err => res.status(400).json(`Error ${err}`));
-});
-
-
-router.route('/add').post((req, res) => {
-    const {
-        name,
-        abbreviation,
-        description,
-        linkToWebSite,
-        image
-    } = req.body;
-
-    const newFaculty = new Faculty({
-        name,
-        abbreviation,
-        description,
-        linkToWebSite,
-        image
-    });
-
-    newFaculty.save()
-        .then((addedElement) => res.json(
-            {
-                message: "Faculty was successfully added", addedElement
-            }
-        ))
-        .catch(err => res.status(400).json(`Error ${err}`));
-});
+router.get('/all', facultyController.getAllFaculties)
+router.get('/', facultyController.getFaculty)
+router.delete('/:id', facultyController.deleteFaculty)
+router.post('/', facultyController.addFaculty)
 
 module.exports = router;
