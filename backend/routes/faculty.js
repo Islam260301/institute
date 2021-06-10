88203b11ed6c96ba11f6888806803090d8eaacfe
islamorozobaev <islam260301kg@gmail.com>
2021-06-11 00:28:38 +0600

    added refs (foreign keys)

--- backend/routes/faculty.js
+++ backend/routes/faculty.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 const facultyController = require('../controllers/facultyController');
 
 
-router.get('/all', facultyController.getAllFaculties)
+router.get('/', facultyController.getAllFaculties)
 router.get('/:id', facultyController.getFaculty)
 router.delete('/:id', facultyController.deleteFaculty)
 router.post('/', facultyController.addFaculty)
-router.put('/:id', facultyController.updateFaculty)
+router.put('/:id', facultyController.editFaculty)
 
 module.exports = router;
