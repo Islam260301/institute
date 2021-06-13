446b69ae25debfa1d925698760554827054ae9b2
islamorozobaev <islam260301kg@gmail.com>
2021-06-14 04:18:28 +0600

    upload image

---
+++ backend/middleWare/upload.js
@@ -0,0 +1,1 @@
+const multer = require('multer');const storage = multer.diskStorage({    destination: function (req, file, callback) {        callback(null, 'upload/');    },    filename: function (req, file, callback) {        callback(null, file.originalname);    }});exports.upload = multer({storage});
--- backend/routes/event.js
+++ backend/routes/event.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 const eventController = require('../controllers/eventController');
-const multer = require('multer');
-const upload = multer({dest: 'uploads/'})
+const {upload} = require("../middleWare/upload");
+
+
 
 router.get('/', eventController.getAllEvents)
 // router.get('/:id', facultyController.getFaculty)
 // router.delete('/:id', facultyController.deleteFaculty)
-router.post('/',upload.single('image'), eventController.addEvent)
+router.post('/', upload.single('image'), eventController.addEvent)
 // router.put('/:id', facultyController.editFaculty)
 
 module.exports = router;
